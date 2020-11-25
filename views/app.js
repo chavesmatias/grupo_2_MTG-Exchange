6c4370525a00777400352f958cf24c50dec8863e
JuanCruz-Serafini <juancruzserafini@yahoo.com.ar>
2020-11-25 16:15:04 -0300

    inicio del puerto

---
+++ views/app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+
+
+
+app.get('/', function(req,res){
+    res.sendFile(__dirname + '/views/index.html')
+})
+
+app.get('/carrito', function(req, res){
+    res.sendFile(__dirname + '/views/carrito.html')
+})
+
+app.listen(3030, () => console.log("Servidor corriendo en el puerto 3030"))
