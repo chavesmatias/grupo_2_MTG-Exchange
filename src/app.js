9fc4a4ef3f8f8b50a11f612cd93fc581f947879a
Carlos German <carlosgermanheineken@gmail.com>
2020-12-18 17:33:02 -0300

    se agrego app.js completo, routes e images

--- src/app.js
+++ src/app.js
@@ -17,7 +17,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
@@ -37,5 +37,9 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+
+
+
 
 module.exports = app;
--- src/routes/index.js
+++ src/routes/index.js
@@ -6,4 +6,40 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/index', function(req, res, next) {
+  res.render('index', { title: 'index' });
+});
+
+router.get('/venta', function(req, res, next) {
+  res.render('venta', { title: 'venta' });
+});
+
+router.get('/registro', function(req, res, next) {
+  res.render('registro', { title: 'registro' });
+});
+
+router.get('/compra', function(req, res, next) {
+  res.render('compra', { title: 'compra' });
+});
+
+router.get('/detalle', function(req, res, next) {
+  res.render('detalle', { title: 'detalle' });
+});
+
+router.get('/carrito', function(req, res, next) {
+  res.render('carrito', { title: 'carrito' });
+});
+
+router.get('/login', function(req, res, next) {
+  res.render('login', { title: 'login' });
+});
+
+router.get('/contacto', function(req, res, next) {
+  res.render('contacto', { title: 'contacto' });
+});
+
+router.get('/busqueda-avanzada', function(req, res, next) {
+  res.render('busqueda-avanzada', { title: 'busqueda-avanzada' });
+});
+
 module.exports = router;
